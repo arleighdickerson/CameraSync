078adf5ee7190f6abd01ebdd1dadc85bfa936373
Arleigh Dickerson <arleigh.dickerson@mmodal.com>
2019-09-11 12:35:13 -0400

    ...

--- src/createDuckRuntime.ts
+++ src/createDuckRuntime.ts
@@ -1,3 +1,4 @@
+import _ from 'lodash';
 import {
   ComposableDuck,
   BaseDuck,
@@ -19,20 +20,20 @@ export interface StoreConfiguration {
     enhancers?: StoreEnhancer[]
 }
 
+class DuckForReducer extends BaseDuck {
+    private readonly _reducer: Reducer;
+
+    constructor(reducer: Reducer, options?: DuckOptions) {
+      options ? super(options) : super();
+      this._reducer = reducer;
+    }
+
+    get reducer() {
+      return this._reducer;
+    }
+}
+
 const createInstance = ({ reducers, middlewares = [], enhancers = [] }: StoreConfiguration): DuckRuntime => {
-  const createDuckForReducer = (reducer: Reducer) => {
-    return class extends BaseDuck {
-      constructor(options: DuckOptions | null = null) {
-        const args = options === null ? [] : [options];
-        super(...args);
-      }
-
-      get reducer() {
-        return reducer;
-      }
-    };
-  };
-
   class RootDuck extends ComposableDuck {
     get rawDucks() {
       const deviceKey = 'devices';
@@ -41,27 +42,26 @@ const createInstance = ({ reducers, middlewares = [], enhancers = [] }: StoreCon
       const deviceDuck = new DeviceDuck(this.getSubDuckOptions(deviceKey));
       const permissionDuck = new PermissionDuck(this.getSubDuckOptions(permissionKey), deviceDuck);
 
+      const reducerDucks = _.mapValues(
+        reducers,
+        (reducer, key) => new DuckForReducer(reducer, this.getSubDuckOptions(key))
+      );
+
       return {
+        ...reducerDucks,
         [deviceKey]:     deviceDuck,
         [permissionKey]: permissionDuck,
       };
-    }
-
-    get quickDucks() {
-      const ducks = {};
-
-      Object.entries(reducers).forEach(([key, reducer]) => {
-        // @ts-ignore
-        ducks[key] = createDuckForReducer(reducer);
-      });
-
-      return ducks;
     }
   }
 
   const rootDuck = new RootDuck();
 
-  const options: DuckRuntimeOptions = { middlewares, enhancers };
+  const options: DuckRuntimeOptions = {
+    middlewares,
+    enhancers,
+  };
+
   return new DuckRuntime(rootDuck, options);
 };
 
@@ -75,4 +75,6 @@ const createFactory = (storeConfiguration: StoreConfiguration) => {
   };
 };
 
-export default (storeConfiguration: StoreConfiguration) => createGhostObject(createFactory(storeConfiguration));
+export default (storeConfiguration: StoreConfiguration) => {
+  return createGhostObject(createFactory(storeConfiguration));
+};
