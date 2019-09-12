b06a5c09eeca495648a1686e77d8bc11db917536
Arleigh Dickerson <arleigh.dickerson@mmodal.com>
2019-09-11 20:10:40 -0400

    adds events package shim

--- src/ioc/index.ts
+++ src/ioc/index.ts
@@ -1,12 +1,16 @@
 import { Container } from 'inversify';
+import emitterModule from 'src/modules/emitter/inversify.module';
 import devicesModule from 'src/modules/devices/inversify.module';
 import permissionsModule from 'src/modules/permissions/inversify.module';
 import { createGhostObject } from '../util/ghostObject';
 
 const createContainer = () => {
   const container = new Container();
+
+  container.load(emitterModule);
   container.load(devicesModule);
   container.load(permissionsModule);
+
   return container;
 };
 
